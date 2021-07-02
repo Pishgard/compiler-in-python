0ea2aa212012d776d294a8712445764d50d3c0a5
Pishgard <alipishgard79@gmail.com>
2021-07-02 13:35:00 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from lexer import Lexer
+from parsers import Parser
+
+text_input = """
+print(4 + 4 - 2);
+"""
+
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text_input)
+
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
